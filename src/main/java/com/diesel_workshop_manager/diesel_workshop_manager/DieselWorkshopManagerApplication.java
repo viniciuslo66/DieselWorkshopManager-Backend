3fa435855c904e4fbbac05443e9fba2045ed8e7c
viniciuslo66 <viniciulo66@gmail.com>
2023-08-16 13:14:10 -0300

    first commit

---
+++ src/main/java/com/diesel_workshop_manager/diesel_workshop_manager/DieselWorkshopManagerApplication.java
@@ -0,0 +1,13 @@
+package com.diesel_workshop_manager.diesel_workshop_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DieselWorkshopManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DieselWorkshopManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diesel_workshop_manager/diesel_workshop_manager/DieselWorkshopManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diesel_workshop_manager.diesel_workshop_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DieselWorkshopManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
