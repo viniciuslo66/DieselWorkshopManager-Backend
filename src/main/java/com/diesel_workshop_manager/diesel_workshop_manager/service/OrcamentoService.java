aabcd047fc34ab0b56161547bba71ea806789070
viniciuslo66 <viniciuslo66@gmail.com>
2024-03-13 10:13:00 -0300

    Aperfeiçoamento dos métodos converter de relatório e orçamento.

--- src/main/java/com/diesel_workshop_manager/diesel_workshop_manager/service/OrcamentoService.java
+++ src/main/java/com/diesel_workshop_manager/diesel_workshop_manager/service/OrcamentoService.java
@@ -2,7 +2,6 @@ package com.diesel_workshop_manager.diesel_workshop_manager.service;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +17,7 @@ import com.diesel_workshop_manager.diesel_workshop_manager.repository.OrcamentoR
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 
+@SuppressWarnings("null")
 @Service
 public class OrcamentoService {
 
@@ -70,70 +70,70 @@ public class OrcamentoService {
   // ------------------------------- converter -------------------------------
 
   private Orcamento converter(OrcamentoDTO dto, Optional<Orcamento> optional) {
-    Orcamento orcamento = Objects.nonNull(optional) ? optional.get() : new Orcamento();
+    Orcamento orcamento = optional.orElse(new Orcamento());
 
     List<Long> ids = dto.getRelatorios();
 
-    if (ids.isEmpty())
-      throw new RegraNegocioException("Nenhum relátório encontrado para os IDs");
+    if (ids.isEmpty()) {
+      throw new RegraNegocioException("Nenhum relatório encontrado para os IDs");
+    }
 
     List<Relatorio> relatorios = relatorioService.findRelatoriosByIds(ids);
-
     orcamento.setRelatorios(relatorios);
 
     if (!relatorios.isEmpty()) {
-
-      // Inicialize as datas com a primeira data do primeiro relatório
-      orcamento.setDataInicio(relatorios.get(0).getDataInicio());
-      orcamento.setDataFim(relatorios.get(0).getDataFim());
-
-      Double totalPrecoServicos = 0.0;
-
-      /*
-       * Itere pela lista de relatórios para encontrar a data mais antiga (dataInicio)
-       * e a data mais recente (dataFim)
-       */
-
-      for (Relatorio relatorio : relatorios) {
-
-        if (relatorio.getDataInicio().before(orcamento.getDataInicio())) {
-          orcamento.setDataInicio(relatorio.getDataInicio());
-        }
-        if (relatorio.getDataFim().after(orcamento.getDataFim())) {
-          orcamento.setDataFim(relatorio.getDataFim());
-        }
-
-        // Iterar pela lista de servicos dentro do Relatorio e somar os preços
-        if (relatorio.getServicos() != null) {
-          for (Map.Entry<Servico, Integer> entry : relatorio.getServicos().entrySet()) {
-            Servico servico = servicoService.findById(entry.getKey().getId());
-            Integer quantidade = entry.getValue();
-
-            if (servico != null) {
-              Double preco = servico.getPrice();
-              totalPrecoServicos += preco * quantidade;
-            }
-
-          }
-        }
-
-        orcamento.setSubtotal(totalPrecoServicos);
-      }
-
-      orcamento.setDesconto(totalPrecoServicos * 0.10);
-      orcamento.setTotalPrice(totalPrecoServicos - (totalPrecoServicos * 0.10));
-
+      calcularSubtotal(orcamento, relatorios);
     } else {
-
-      orcamento.setSubtotal(null);
-      orcamento.setDesconto(null);
-      orcamento.setTotalPrice(null);
-      orcamento.setDataInicio(null);
-      orcamento.setDataFim(null);
-      throw new RegraNegocioException("A lista de relatórios está vazia");
-      
+      ajustarValoresOrcamentoVazio(orcamento);
     }
 
     return orcamento;
+  }
+
+  private void calcularSubtotal(Orcamento orcamento, List<Relatorio> relatorios) {
+    double totalPrecoServicos = 0.0;
+
+    for (Relatorio relatorio : relatorios) {
+      totalPrecoServicos += calcularPrecoServicos(relatorio);
+      ajustarDatas(orcamento, relatorio);
+    }
+
+    orcamento.setSubtotal(totalPrecoServicos);
+    orcamento.setDesconto(totalPrecoServicos * 0.10);
+    orcamento.setTotalPrice(totalPrecoServicos - (totalPrecoServicos * 0.10));
+  }
+
+  private double calcularPrecoServicos(Relatorio relatorio) {
+    double precoTotal = 0.0;
+    Map<Servico, Integer> servicos = relatorio.getServicos();
+
+    if (servicos != null) {
+      for (Map.Entry<Servico, Integer> entry : servicos.entrySet()) {
+        Servico servico = entry.getKey();
+        Integer quantidade = entry.getValue();
+        double preco = servico.getPrice();
+        precoTotal += preco * quantidade;
+      }
+    }
+
+    return precoTotal;
+  }
+
+  private void ajustarDatas(Orcamento orcamento, Relatorio relatorio) {
+    if (relatorio.getDataInicio().before(orcamento.getDataInicio())) {
+      orcamento.setDataInicio(relatorio.getDataInicio());
+    }
+    if (relatorio.getDataFim().after(orcamento.getDataFim())) {
+      orcamento.setDataFim(relatorio.getDataFim());
+    }
+  }
+
+  private void ajustarValoresOrcamentoVazio(Orcamento orcamento) {
+    orcamento.setSubtotal(null);
+    orcamento.setDesconto(null);
+    orcamento.setTotalPrice(null);
+    orcamento.setDataInicio(null);
+    orcamento.setDataFim(null);
+    throw new RegraNegocioException("A lista de relatórios está vazia");
   }
 }
--- src/main/java/com/diesel_workshop_manager/diesel_workshop_manager/service/RelatorioService.java
+++ src/main/java/com/diesel_workshop_manager/diesel_workshop_manager/service/RelatorioService.java
@@ -1,11 +1,10 @@
 package com.diesel_workshop_manager.diesel_workshop_manager.service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,6 +21,7 @@ import com.diesel_workshop_manager.diesel_workshop_manager.repository.RelatorioR
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 
+@SuppressWarnings("null")
 @Service
 public class RelatorioService {
 
@@ -93,21 +93,28 @@ public class RelatorioService {
       throw new RegraNegocioException("Nenhum serviço encontrado para os IDs informados.");
     }
 
-    List<Servico> servicos = servicoService.findServicosByIds(new ArrayList<>(idsQuantidades.keySet()));
+    // Obter serviços do banco de dados com base nos IDs
+    List<Servico> servicos = idsQuantidades.keySet().stream()
+        .map(servicoService::findById)
+        .peek(servico -> {
+          if (servico == null) {
+            throw new RegraNegocioException("Serviço não encontrado para o ID: " + servico.getId());
+          }
+        })
+        .collect(Collectors.toList());
 
-    // Configure o mapa quantidadeServicos em Relatorio com base em idsQuantidades
-    Map<Servico, Integer> servicosQuantidade = new HashMap<>();
-    
-    for (Servico servico : servicos) {
-      servicosQuantidade.put(servico, idsQuantidades.get(servico.getId()));
-    }
+    // Criar mapa de serviços e suas quantidades
+    Map<Servico, Integer> servicos_quantidades = servicos.stream()
+        .collect(Collectors.toMap(
+            servico -> servico,
+            servico -> idsQuantidades.get(servico.getId())));
 
     relatorio.setDataInicio(dto.getDataInicio());
     relatorio.setDataFim(dto.getDataFim());
     relatorio.setCliente(cliente);
     relatorio.setUsuario(usuario);
     relatorio.setVeiculo(veiculo);
-    relatorio.setServicos(servicosQuantidade);
+    relatorio.setServicos(servicos_quantidades);
 
     return relatorio;
   }
